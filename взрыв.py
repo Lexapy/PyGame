8af291575f2742d662549cac5c371512583db13a
Lexapy <a8F34Xd6B5>
2022-01-13 22:42:15 +0500

    people walk

--- взрыв.py
+++ взрыв.py
@@ -17,43 +17,39 @@ def load_image(name):
     return pygame.image.load(fullname)
 
 
-class Worker(pygame.sprite.Sprite):
-    steps = [load_image(f"{i}.png") for i in range(8)]
+class Bomb(pygame.sprite.Sprite):
+    image = load_image("bomb.png")
 
     def __init__(self, *group):
         super().__init__(*group)
-
-        self.image = Worker.steps[0]
+        self.image = Bomb.image
         self.rect = self.image.get_rect()
-        self.rect.x = 5
-        self.rect.y = randrange(height - self.rect[3])
+        self.rect.x = randrange(width)
+        self.rect.y = randrange(height)
 
     def update(self):
-        # сюда таймер
-        self.rect = self.rect.move(5, 0)
-        self.image = self.steps[(self.steps.index(self.image) + 1) % 8]
+        self.rect = self.rect.move(randrange(3) - 1,
+                                   randrange(3) - 1)
 
 
 def main():
     all_sprites = pygame.sprite.Group()
-    for _ in range(2):
-        Worker(all_sprites)
+    for _ in range(10):
+        Bomb(all_sprites)
     clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                for i in range(40):
-                    Worker(all_sprites)
         screen.fill((255, 255, 255))
         all_sprites.draw(screen)
         all_sprites.update()
-        clock.tick(12)  # переделать смену кадров по таймеру
+        clock.tick(60)
         pygame.display.flip()
     pygame.quit()
 
 
 if __name__ == '__main__':
     sys.exit(main())
+
